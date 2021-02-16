a11ff5917f89358d11033dd067be1c6bceeecad4
Danang Eko Alfianto <danangekal@gmail.com>
2021-02-16 18:00:23 +0700

    Update title on navbar components

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav>
       <Head>
-        <title>Create Next App</title>
+        <title>Next Typescript PWA Stater</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
